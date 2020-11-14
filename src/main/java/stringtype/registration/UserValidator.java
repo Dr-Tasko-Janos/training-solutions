84ecd50edd6c94d46fe634ea2e4637e21840c943
Dr. Taskó János <tasko.janos@protonmail.ch>
2020-11-14 23:00:54 +0100

    Commit UserValidator

---
+++ src/main/java/stringtype/registration/UserValidator.java
@@ -0,0 +1,17 @@
+package stringtype.registration;
+
+public class UserValidator {
+
+    public boolean isValidUsername(String username) {
+        return (username.length() > 0) ? true : false;
+    }
+
+    public boolean isValidPassword(String password1, String password2) {
+
+        return (password1.length() >= 8 && password2.length() >= 8 && password1.equals(password2)) ? true : false;
+    }
+
+    public boolean isValidEmail(String email) {
+        return ((email.indexOf("@") >= 1) && (email.indexOf(".") > email.indexOf("@") + 1) && (email.indexOf(".") < (email.length() - 1))) ? true : false;
+    }
+}
