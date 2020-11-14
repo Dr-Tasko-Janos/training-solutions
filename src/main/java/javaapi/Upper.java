0c895084114853483675bff6b4dbf92bb8d27ca5
Dr. Taskó János <tasko.janos@protonmail.ch>
2020-11-14 20:15:41 +0100

    Commit Upper

---
+++ src/main/java/javaapi/Upper.java
@@ -0,0 +1,12 @@
+package javaapi;
+
+public class Upper {
+    public static void main(String[] args) {
+        String greeting = "Hello World!";
+
+        String upperGreeting = greeting.toUpperCase();
+
+        System.out.println(greeting);
+        System.out.println(upperGreeting);
+    }
+}
