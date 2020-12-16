309071dac8cd70e6c9aa475a49ea7f4c8b82d0fa
Dr. Taskó János <tasko.janos@protonmail.ch>
2020-12-16 20:23:59 +0100

    Commit ChangeLetter.java

--- src/main/java/week05d02/ChangeLetter.java
+++ src/main/java/week05d02/ChangeLetter.java
@@ -6,19 +6,18 @@ import java.util.List;
 public class ChangeLetter {
 
     public static String changeVowels(String text) {
-        final List<Character> vowels = Arrays.asList('a','A', 'e', 'E', 'i', 'I', 'o', 'O', 'u',  'U');
-        char[] textArray = text. toCharArray();
+        final List<Character> vowels = Arrays.asList('a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U');
+        char[] textArray = text.toCharArray();
         for (int i = 0; i < textArray.length; i++) {
-            if (vowels.contains(textArray[i])){
+            if (vowels.contains(textArray[i])) {
                 textArray[i] = '*';
             }
         }
         return new String(textArray);
     }
+
     public static void main(String[] args) {
         System.out.println(changeVowels("HEllo"));
-
-
     }
 }
 
