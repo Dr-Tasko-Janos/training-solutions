0e45a7177800a4ef9d92377c357a442a9569dbdc
Dr. Taskó János <tasko.janos@protonmail.ch>
2020-12-17 00:10:56 +0100

    Commit ListCounterTest.java - improved

--- src/test/java/week05d03/ListCounterTest.java
+++ src/test/java/week05d03/ListCounterTest.java
@@ -1,6 +1,7 @@
 package week05d03;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
@@ -8,24 +9,42 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ListCounterTest {
+
     ListCounter testCase = new ListCounter();
 
     List<String> myList1 = Arrays.asList("Neque", "porro", "quisquam", "est", "qui",
-            "dolorem", "ipsum", "quia", "dolor", "sit", "amet", "consectetur", "adipisci", "velit");
+            "dolorem", "ipsum", "quia", "dolor", "sit", "amet", "consectetur", "Adipisci", "velit");
 
     List<String> myList2 = Arrays.asList("Neque", "porro", "quisquam", "est", "qui",
             "dolorem", "ipsum", "quia", "dolor", "sit", "amet", "consectetur", "adipisci", "velit", "Anna");
 
 
     @Test
-    public void testListCounter1() {
+    public void testListCounter1(){
+        assertEquals(2, testCase.listCounter(myList1));
+    }
+
+
+    @Test
+    public void testListCounter2() {
+        assertEquals(3, testCase.listCounter(myList2));
+    }
+
+
+
+
+/*
+    //In case if last character matching is important too
+
+    @Test
+    public void testListCounter3() {
 
         assertEquals(3, testCase.listCounter(myList1)); //After counting manually
     }
 
     @Test
-    public void testListCounter2() {
+    public void testListCounter4() {
         assertEquals(4, testCase.listCounter(myList2));
-    }
+    }*/
 }
 
