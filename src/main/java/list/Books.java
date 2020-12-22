b7592764c45bb4f8bd8a8b29e060c8a089681e56
Dr. Taskó János <tasko.janos@protonmail.ch>
2020-12-22 02:22:14 +0100

    Books.java - Bonus Task2 solution has been added

--- src/main/java/list/Books.java
+++ src/main/java/list/Books.java
@@ -37,5 +37,35 @@ public class Books {
         System.out.println(myBooks.getTitles());
         System.out.println("The result for \"Az e\" prefix: " + myBooks.findAllByPrefix("Az e"));
         System.out.println("The result for \"Az \" prefix: " + myBooks.findAllByPrefix("Az "));
+
+        //Testing Bonus Task2 implementation:
+
+        System.out.println("\n\nTesting Bonus Task2 implementation:");
+        System.out.println("Here is the original List: " + myBooks.getTitles());
+        System.out.println("Content of books List after using the removeByPrefix method with the parameter of: \"Na\": ");
+        myBooks.removeByPrefix("Na ");
+        System.out.print(myBooks.getTitles());
+        System.out.println("As you can see, nothing has happened!");
+
+        System.out.println("Trying with removeAll method implemented to the removeByPrefixWithRemoveAll method: ");
+        System.out.println("Here is the original myBooks List: " + myBooks.getTitles());
+        myBooks.removeByPrefixWithRemoveAll("Na");
+        System.out.println("Content of books List after using the removeByPrefixWithRemoveAll method with the parameter of: \"Na\": " + myBooks.getTitles());
+
+    }
+
+    //Bonus Task2 implementation:
+
+    public void removeByPrefix(String prefix) {
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).startsWith(prefix)) {
+                books.remove(i);
+            }
+        }
+    }
+
+    //The final solution for the Bonus Task2: :-)
+    public void removeByPrefixWithRemoveAll(String prefix) {
+        books.removeAll(findAllByPrefix(prefix));
     }
 }
