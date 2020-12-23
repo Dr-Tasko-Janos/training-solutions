5d846f17d49ca52dba0764fdfe897aa1f345a355
Dr. Taskó János <tasko.janos@protonmail.ch>
2020-12-23 22:35:09 +0100

    Member.java - SocialNetwork toString() method-, and Bonus task has been added.

--- src/main/java/references/socialnetwork/Member.java
+++ src/main/java/references/socialnetwork/Member.java
@@ -24,4 +24,29 @@ public class Member {
     public void connectMember(Member member) {
         connections.add(member);
     }
+
+    public String getConnectedName() {
+        String markedNames = "";
+        for (int i = 0; i < connections.size(); i++) {
+            markedNames += (i == connections.size() - 1) ? connections.get(i).getName() : connections.get(i) + ", ";
+        }
+        return markedNames;
+    }
+
+    @Override
+    public String toString() {
+        return name + " marked as firend the following memebers:" + getConnectedName() + "\n";
+    }
+
+    /*
+    Bonus task:
+    It will not working. The list contains "Member" type elements, and we have to use their getName() methods.
+
+    @Override
+    public String toString() {
+        return name + " " + connections.toString();
+    }*/
+
+
 }
+
