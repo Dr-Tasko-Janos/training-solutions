e9c0d93399636373a63137e4d20a8fa052fac24c
Dr. Taskó János <tasko.janos@protonmail.ch>
2020-12-24 12:56:46 +0100

    MathAlgorithmsTest.java - because MathAlgorithms.java class has been renamed

--- src/test/java/week07d01/MathAlgorithmsTest.java
+++ src/test/java/week07d01/MathAlgorithmsTest.java
@@ -3,58 +3,58 @@ package week07d01;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static week07d01.MathAlogrithms.isPrime;
+import static week07d01.MathAlgorithms.isPrime;
 
 public class MathAlgorithmsTest {
 
     //Prime section
     @Test
     public void mathAlgorithmsTest2() {
-        MathAlogrithms mathAlogrithms = new MathAlogrithms();
+        MathAlgorithms mathAlgorithms = new MathAlgorithms();
         assertEquals(true, isPrime(2));
     }
 
     @Test
 
     public void mathAlgorithmsTest3() {
-        MathAlogrithms mathAlogrithms = new MathAlogrithms();
+        MathAlgorithms mathAlgorithms = new MathAlgorithms();
         assertEquals(true, isPrime(3));
     }
 
     @Test
     public void mathAlgorithmsTest7() {
-        MathAlogrithms mathAlogrithms = new MathAlogrithms();
+        MathAlgorithms mathAlgorithms = new MathAlgorithms();
         assertEquals(true, isPrime(7));
     }
 
     @Test
     public void mathAlgorithmsTest13() {
-        MathAlogrithms mathAlogrithms = new MathAlogrithms();
+        MathAlgorithms mathAlgorithms = new MathAlgorithms();
         assertEquals(true, isPrime(13));
     }
 
     @Test
     public void mathAlgorithmsTest10007() {
-        MathAlogrithms mathAlogrithms = new MathAlogrithms();
+        MathAlgorithms mathAlgorithms = new MathAlgorithms();
         assertEquals(true, isPrime(10007));
     }
 
     //Non-prime section
     @Test
     public void mathAlgorithmsTest4() {
-        MathAlogrithms mathAlogrithms = new MathAlogrithms();
+        MathAlgorithms mathAlgorithms = new MathAlgorithms();
         assertEquals(false, isPrime(4));
     }
 
     @Test
     public void mathAlgorithmsTest16() {
-        MathAlogrithms mathAlogrithms = new MathAlogrithms();
+        MathAlgorithms mathAlgorithms = new MathAlgorithms();
         assertEquals(false, isPrime(16));
     }
 
     @Test
     public void mathAlgorithmsTest10008() {
-        MathAlogrithms mathAlogrithms = new MathAlogrithms();
+        MathAlgorithms mathAlgorithms = new MathAlgorithms();
         assertEquals(false, isPrime(10008));
     }
 
@@ -62,7 +62,7 @@ public class MathAlgorithmsTest {
 
     @Test
     public void mathAlgorithmsTestMinus2() {
-        MathAlogrithms mathAlogrithms = new MathAlogrithms();
+        MathAlgorithms mathAlgorithms = new MathAlgorithms();
         assertEquals(true, isPrime(-2));
     }
 }
