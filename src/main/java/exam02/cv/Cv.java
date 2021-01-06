9b5f952bc2534838d0063b970868e6442ddb67a2
Dr. Taskó János <tasko.janos@protonmail.ch>
2021-01-06 19:51:57 +0100

    Cv.java - Formázott - KÉSZ

--- src/main/java/exam02/cv/Cv.java
+++ src/main/java/exam02/cv/Cv.java
@@ -4,55 +4,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-
-
 public class Cv {
+
     private String name;
     private List<Skill> skills = new ArrayList<>();
 
 
-    public Cv(String name){
+    //Constructor:
+    public Cv(String name) {
         this.name = name;
     }
 
-    public String getName(){
+
+    //Setters:
+    public void addSkills(String... complexName) {
+
+        List<Skill> newSkills = new ArrayList<>();
+
+        for (int i = 0; i < complexName.length; i++) {
+            String basic = complexName[i];
+
+            String name = basic.trim().substring(0, getSkills().indexOf("("));
+
+            String level = basic.substring(basic.indexOf(")") - 1);
+            int levelX = Integer.parseInt(level);
+
+            newSkills.add(new Skill(name, levelX));
+        }
+    }
+
+
+    public void addSkills(List<Skill> skills) {
+        for (Skill item : skills) {
+            this.skills.add(new Skill(item.getName(), item.getLevel()));
+        }
+    }
+
+
+    //Getters:
+    public String getName() {
         return name;
     }
 
 
-    public void addSkills(String... complexName){
-        List<Skill> newSkills = new ArrayList<>();
-        for(int i = 0; i < complexName.length; i++){
-           String basic = complexName[i];
-           String name = basic.trim().substring(0, getSkills().indexOf("("));
-           String level = basic.substring(basic.indexOf(")") - 1);
-           int levelX = Integer.parseInt(level);
-           newSkills.add(new Skill(name, levelX));
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public List<Skill> getSkills(){
+    public List<Skill> getSkills() {
         return skills;
     }
-
-    public void addSkills(List<Skill> skills) {
-        for (Skill item : skills) {
-            this.skills.add(new Skill(item.getName(),item.getLevel()));
-        }
-    }
-
 
 
     public Cv(String name, String skillName, int level) {
@@ -60,6 +57,8 @@ public class Cv {
         skills.add(new Skill(skillName, level));
     }
 
+
+    //Other methods
     public String findSkillLevelByName(String skillName) {
         String returnString = "";
         for (Skill item : skills) {
@@ -70,8 +69,8 @@ public class Cv {
         return null;
     }
 
-    class SkillNotFoundException extends Exception{
 
+    //Custom exception:
+    class SkillNotFoundException extends Exception {
     }
-
 }
