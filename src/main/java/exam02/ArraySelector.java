5a44f28d27a93cd5e9f63ecd72696655e62b74fb
Dr. Taskó János <tasko.janos@protonmail.ch>
2021-01-06 18:25:09 +0100

    ArraySelectorJava - Kész

--- src/main/java/exam02/ArraySelector.java
+++ src/main/java/exam02/ArraySelector.java
@@ -1,4 +1,15 @@
 package exam02;
 
 public class ArraySelector {
+
+    public String selectEvens(int[] stringArray) {
+        String selected = "[";
+        if (stringArray.length == 0) {
+            return "";
+        }
+        for (int i = 0; i < stringArray.length; i += 2) {
+            selected += (i >= stringArray.length - 2) ? stringArray[i] + "]" : stringArray[i] + ", ";
+        }
+        return selected;
+    }
 }
