93f93ab51c37b7e39306fa29c4d236515401712e
Dr. Taskó János <tasko.janos@protonmail.ch>
2021-01-06 18:48:17 +0100

    Skill.java - Kész

---
+++ src/main/java/exam02/cv/Skill.java
@@ -0,0 +1,19 @@
+package exam02.cv;
+
+public class Skill {
+    private String skillName;
+    private int level;
+
+    public Skill(String skillName, int level) {
+        this.skillName = skillName;
+        this.level = level;
+    }
+
+    public String getName() {
+        return skillName;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+}
