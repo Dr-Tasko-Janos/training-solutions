966c37b6d4f1660f3ef2921609df8a6b5b42698c
Dr. Taskó János <tasko.janos@protonmail.ch>
2021-01-17 21:41:35 +0100

    ActivityType.java

---
+++ src/main/java/activity/ActivityType.java
@@ -0,0 +1,16 @@
+package activity;
+
+public enum ActivityType {
+
+    BIKING(true), HIKING(true), RUNNING(true), BASKETBALL(false);
+
+    private boolean hasTrack;
+
+    ActivityType(boolean hasTrack) {
+        this.hasTrack = hasTrack;
+    }
+
+    public boolean hasTrack() {
+        return hasTrack;
+    }
+}
