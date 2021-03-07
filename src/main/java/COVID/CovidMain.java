7acb15940cc5f5f242e4e07b61cf49fa0a20eb5b
Dr. Taskó János <tasko.janos@protonmail.ch>
2021-03-07 01:53:48 +0100

    CovidMain.java - near done

---
+++ src/main/java/COVID/CovidMain.java
@@ -0,0 +1,81 @@
+package COVID;
+
+import org.mariadb.jdbc.MariaDbDataSource;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class CovidMain {
+
+    public static void main(String[] args) {
+
+        MariaDbDataSource dataSource = new MariaDbDataSource();
+        try {
+            dataSource.setUrl("jdbc:mariadb://localhost:3306:/covid?useUnicode=true");
+            dataSource.setUser("covidUser");
+            dataSource.setPassword("covidUser");
+        } catch (SQLException sqle) {
+            throw new IllegalArgumentException("DataSource create error", sqle);
+        }
+
+        int chosenOption;
+
+        while (true) {
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("\n\n\n\t\t\t *** OPTIONS ***\n");
+            System.out.println("0. EXIT(Kilépés a porgramból)\n");
+            System.out.println("1. Registration(Regisztráció)");
+            System.out.println("2. Mass Registration(Tömeges regisztráció)");
+            System.out.println("3. Generate Query List(Generálás)");
+            System.out.println("4. Vaccination(Oltás)");
+            System.out.println("5. Failed Vaccination(Oltás meghiúsulás)");
+            System.out.println("6. Riport");
+
+            System.out.print("\nType the number of the chosen option from above: ");
+
+            inner:
+            while (true) {
+                if (scanner.hasNextInt()) {
+                    chosenOption = scanner.nextInt();
+
+                    if (chosenOption >= 0 && chosenOption <= 6) {
+
+                        switch (chosenOption) {
+                            case 1:
+                                new SingleRegistration().singleRegistration();
+                                scanner.nextLine();
+                                break inner;
+                            case 2:
+                                new MassRegistration().massRegistraton();
+                                scanner.nextLine();
+                                break inner;
+                            case 3:
+                                new GenerateListToDailyVaccination().generateListToDailyVaccination();
+                                scanner.nextLine();
+                                System.out.println();
+                                break inner;
+                            case 0:
+                                System.out.println("The program is Exit.");
+                                scanner.close();
+                                System.exit(0);
+                        }
+                    } else {
+                        System.out.printf("\nInput must between 0(included) and 6(included). The given value was: %d.\n" +
+                                "Try again or press 0 to Exit the program.\n\n", chosenOption);
+                        break inner;
+                    }
+                } else {
+                    System.out.println("\n\nInteger input is required! You entered:Try again or press 0 to EXIT!");
+                    break inner;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
